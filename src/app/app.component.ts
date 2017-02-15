69fc2ef91eb8031b678eb6042208cb2e378d9017
tmucha <tmucha@cyfrowypolsat.pl>
2017-02-15 23:47:12 +0100

    article model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Article} from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,21 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article("Title 1", "http://article1.com", 3),
+      new Article("Title 2", "http://article2.com", 2),
+      new Article("Title 3", "http://article3.com", 1)
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Add link ${title.value} and ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
 
     return true
   };
